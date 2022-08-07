589673e5be5532ac65f48b8a880d9f2c18797b40
Showndharyaa <showndharyaa27@gmail.com>
2022-08-07 15:34:19 -0500

    Syntax correction

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
-	public static void main(string args[])
+	public static void main(String args[])
 	{
-		system.out.println("Hello,World");
+		System.out.println("Hello,World");
 	}
 }
